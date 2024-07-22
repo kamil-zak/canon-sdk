15d94952bee6b7e27d93c144f6ce91a92a3ca504
Kamil Żak <zak.kamil.pl@gmail.com>
2024-07-22 10:21:31 +0200

    Create app

--- app/src/CameraEvent.cpp
+++ app/src/CameraEvent.cpp
@@ -9,7 +9,7 @@ EdsError downloadImage(EdsDirectoryItemRef directoryItem, EdsVoid *context);
 EdsError EDSCALLBACK handleObjectEvent(EdsObjectEvent event, EdsBaseRef object, EdsVoid *context)
 {
 	EdsError err = EDS_ERR_OK;
-	std::cout << "ObjectEvent" << std::hex << event << std::endl;
+	// std::cout << "ObjectEvent" << std::hex << event << std::endl;
 	switch (event)
 	{
 	case kEdsObjectEvent_DirItemRequestTransfer:
@@ -33,7 +33,7 @@ EdsError EDSCALLBACK handlePropertyEvent(
 	EdsUInt32 inParam,
 	EdsVoid *inContext)
 {
-	std::cout << "StateEvent" << std::hex << inEvent << std::endl;
+	// std::cout << "PropertyEvent" << std::hex << inEvent << std::endl;
 	EdsError err = EDS_ERR_OK;
 	// do something
 	return err;
@@ -41,7 +41,7 @@ EdsError EDSCALLBACK handlePropertyEvent(
 
 EdsError EDSCALLBACK handleSateEvent(EdsStateEvent event, EdsUInt32 parameter, EdsVoid *context)
 {
-	std::cout << "StateEvent" << std::hex << event << std::endl;
+	// std::cout << "StateEvent" << std::hex << event << std::endl;
 	EdsError err = EDS_ERR_OK;
 	// do something
 	return err;
@@ -49,6 +49,6 @@ EdsError EDSCALLBACK handleSateEvent(EdsStateEvent event, EdsUInt32 parameter, E
 
 
 EdsError  EDSCALLBACK handleCameraAdded(EdsVoid *inContext ) {
-	std::cout << "CameraAdded" << std::endl;
+	// std::cout << "CameraAdded" << std::endl;
 	return EDS_ERR_OK;
 };
